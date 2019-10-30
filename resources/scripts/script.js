459420790e189b2fbd165785c271b3fa8870b758
Andreas Carlsson <andreas95carlsson@gmail.com>
2019-10-30 07:10:03 +0100

    Added clock and linked to .js file

---
+++ resources/scripts/script.js
@@ -0,0 +1,8 @@
+const fullDate = new Date();
+
+const hours = fullDate.getHours();
+const minutes = fullDate.getMinutes();
+
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minutes').innerHTML = minutes;
+       
