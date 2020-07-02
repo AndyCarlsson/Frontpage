8191c843771684720009d316ecfca85d5c0bf7f8
AndyCarlsson <andreas95carlsson@gmail.com>
2020-07-02 21:04:01 +0200

    Started with css grid

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,44 +1,56 @@
-
-
-function showDate(){
-  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
+function showDate() {
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Dec',
+  ];
   const d = new Date();
-  document.getElementById("month").innerHTML = monthNames[d.getMonth()] + " ";
-  document.getElementById("day").innerHTML = d.getDate();
+  document.getElementById('month').innerHTML = monthNames[d.getMonth()] + ' ';
+  document.getElementById('day').innerHTML = d.getDate();
 }
 showDate();
 
-function clock(){
+function clock() {
   var fullTime = new Date();
   var hours = fullTime.getHours();
   var minutes = fullTime.getMinutes();
   var seconds = fullTime.getSeconds();
 
-  if (hours < 10){
-    hours = "0" + hours;
+  if (hours < 10) {
+    hours = '0' + hours;
   }
-  if (minutes < 10){
-    minutes = "0" + minutes;
+  if (minutes < 10) {
+    minutes = '0' + minutes;
   }
-  if(seconds < 10){
-    seconds = "0" + seconds
+  if (seconds < 10) {
+    seconds = '0' + seconds;
   }
   document.getElementById('hour').innerHTML = hours;
   document.getElementById('minutes').innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-};
-setInterval(clock, 1000)
+  document.getElementById('seconds').innerHTML = seconds;
+}
+setInterval(clock, 1000);
 
-function getWeather(){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=Gothenburg&units=metric&appid=09b64160c45d82d5322b8d503c8b309d')
-    .then(response => response.json())
-    .then(data => {
+function getWeather() {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Gothenburg&units=metric&appid=09b64160c45d82d5322b8d503c8b309d'
+  )
+    .then((response) => response.json())
+    .then((data) => {
       var tempValue = data['main']['temp'];
 
       document.getElementById('degrees').innerHTML = tempValue;
-      document.getElementById('weather-icon').src = 'http://openweathermap.org/img/wn/10d@2x.png';
-    }) 
-};
+      document.getElementById('weather-icon').src =
+        'http://openweathermap.org/img/wn/10d@2x.png';
+    });
+}
 getWeather();
-
-
